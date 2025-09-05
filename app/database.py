9c22842b69b457f8128d346f9ae9d1d8922a2701
prashannapudasaini1 <pudasainiprashanna1@gmail.com>
2025-09-05 13:15:51 +0545

    add a config.py and change the name of Database.

--- app/database.py
+++ app/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:prashanna@localhost/blog'
+from app.config import settings
+
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:{settings.database_password}@"
+    f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- routers/oauth2.py
+++ routers/oauth2.py
@@ -8,18 +8,22 @@ from app.config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
         id = payload.get("user_id")
 
         if id is None:
