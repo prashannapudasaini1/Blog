baec87a3ce71a5536cfd6d6eb83af1c0f987345e
prashannapudasaini1 <pudasainiprashanna1@gmail.com>
2025-09-05 13:30:25 +0545

    add the file name config.py and updated the required details

--- app/database.py
+++ app/database.py
@@ -1,16 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from .config import settings  #
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:prashanna@localhost/blog'
+SQLALCHEMY_DATABASE_URL = (
+    f'postgresql://{settings.database_username}:'
+    f'{settings.database_password}@{settings.database_hostname}:'
+    f'{settings.database_port}/{settings.database_name}'
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
 
 def get_db():
     db = SessionLocal()
